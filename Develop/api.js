1b3dbd99521811ef9bd9464d59b882d582745c06
Jordan M <jordan.muliaman@gmail.com>
2020-05-13 19:55:04 -0700

    add notes to js

--- Develop/api.js
+++ Develop/api.js
@@ -1,9 +1,12 @@
 const axios = require('axios')
 
+// make variable for api
 const api = {
   userName(user) {
+    // make request for GitHub user name
     return axios.get(`https://api.github.com/users/${user}`)
       .catch(function (error) {
+        // handle error
         console.log(error);
       })
   }
